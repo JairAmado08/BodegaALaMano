d498923c7f74128b21cf8d1b4ba3851c47c81061
JairAmado08 <jairrenatoamadodiaz@gmail.com>
2025-09-18 23:15:39 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
-a
+# ----------------------------
+# Configuración de la App
+# ----------------------------
+st.set_page_config(
+    page_title="Bodega ALM - Inventario", 
+    layout="wide",
+    page_icon="📦",
+    initial_sidebar_state="expanded"
+)
